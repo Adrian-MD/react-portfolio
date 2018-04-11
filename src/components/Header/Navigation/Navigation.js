d42962e7642cafd0b2e5ed6f63c2fbb3b1ca92a5
Adrian <a.m.davila91@gmail.com>
2018-04-11 12:41:01 -0400

    navbar code restructure

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -3,6 +3,32 @@ import logo from '../logo-concept-square-inverse.png';
 import logo1 from '../calig-dark.png';
 import logo2 from '../calig-light.png';
 
+const navlinks = [
+    {
+        id: 1,
+        link: "#about",
+        scroll: "true",
+        title: "About",
+    },
+    {
+        id: 2,
+        link: "#work",
+        scroll: "true",
+        title: "Work",
+    },
+    {
+        id: 3,
+        link: "#tools",
+        scroll: "true",
+        title: "Tools",
+    },
+    {
+        id: 4,
+        link: "#contact",
+        scroll: "true",
+        title: "Contact",
+    },
+]
 const Navigation = () => {
     return(
         <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
@@ -10,10 +36,13 @@ const Navigation = () => {
                 <a href="" className="uk-navbar-item uk-logo"><img src={logo} alt="Logo" /></a>
                 <div className="uk-navbar-right">
                     <ul className="uk-navbar-nav" uk-scrollspy-nav="closest: li; scroll: true">
-                        <li className=""><a href="#about" uk-scroll="true">About</a></li>
-                        <li className=""><a href="#work" uk-scroll="true">Work</a></li>
-                        <li className=""><a href="#tools" uk-scroll="true">Tools</a></li>
-                        <li className=""><a href="#contact" uk-scroll="true">Contact</a></li>
+                        {
+                            navlinks.map((nav, i) => {
+                                return (
+                                    <li className=""><a href={nav.link} uk-scroll={nav.scroll}>{nav.title}</a></li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </nav>
