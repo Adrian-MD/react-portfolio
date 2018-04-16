da6148c3b03a3986d01c380f867ccdbaeac28650
Adrian <a.m.davila91@gmail.com>
2018-04-15 22:59:04 -0400

    quick check

--- src/components/Main/Work/Work.js
+++ src/components/Main/Work/Work.js
@@ -4,46 +4,53 @@ import WorkCard from './WorkCard.js';
 const projects = [
     {
         id: 1,
-        imageUrl: "",
+        imageUrl: "../../images/my-site.png",
         title: "This Website",
         desc: "This website was created using React.js to get a better sense of building within the React environment. It also uses Node.js, Express.js, UIKit, FontAwesome, Google Analytics, and Google Captcha. The source code can be found on my GitHub.",
         proUrl: "#"
     },
     {
         id: 2,
-        imageUrl: "",
+        imageUrl: "../../images/nj-law.png",
         title: "NJ Law Connect - Responsive Makeover",
         desc: "While working with my previous employer, I tackled this project to make this website mobile-friendly. The website uses a WordPress theme with many plugins. Using FTP, I customized the design of the site to make all current and future pages responsive.",
         proUrl: "https://njlawconnect.com"
     },
     {
         id: 3,
-        imageUrl: "",
+        imageUrl: "../../images/st-johns.png",
         title: "St. John's United Methodist Church",
         desc: "Built within a proprietary platform of my previous employer, I was the Project Manager and Developer for this site. I created a more modern design on a platform with a bit of an older design methodology. The sections like the header, banner, and areas of content are PHP modules that were customized by myself and integrated into the platform's backend for reuse with future clients.",
         proUrl: "http://www.stjohnsivyland.com/"
     },
     {
         id: 4,
-        imageUrl: "",
+        imageUrl: "../../images/at-home-med.png",
         title: "At Home Medical",
         desc: "Client from previous employer. Wanted to modernize the site, make it mobile friendly, and have more control. I designed the site structure, homepage and inner page layout customizations. Everything is editable by the client, a level of customization I provided that is not typical of users on the platform.",
         proUrl: "http://www.athomemedical.org/"
     },
     {
         id: 5,
-        imageUrl: "",
+        imageUrl: "../../images/face-rec-app.png",
         title: "Face Recognizing React App",
         desc: "App built to recognize faces in uploaded images. App was created as part of an online course. The app was built in React.js and uses the ClarifAI face detection API, Node, Express, and PostgreSQL.",
         proUrl: "https://face-recognizing-app.herokuapp.com/"
     },
     {
         id: 6,
-        imageUrl: "",
-        title: "Robofriends React App",
-        desc: "React app that searches through a database of existing users in real-time. The app uses the Robohash API and was the first React app I built as part of an online course.",
-        proUrl: "https://google.com"
+        imageUrl: "../../images/quote-gen.png",
+        title: "Developer Quote Generator",
+        desc: "A simple designer quote generator that changes quotes and colors on click.",
+        proUrl: "https://adrian-md.github.io/api-practice/"
     },
+    // {
+    //     id: 7,
+    //     imageUrl: "../../images/data-concept.png",
+    //     title: "Robofriends React App",
+    //     desc: "React app that searches through a database of existing users in real-time. The app uses the Robohash API and was the first React app I built as part of an online course.",
+    //     proUrl: "https://google.com"
+    // },
 ]
 const Work = () => {
     return(
