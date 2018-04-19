828a2cc17bea5e8461f46ed2f79e4bb87c22672e
Adrian <a.m.davila91@gmail.com>
2018-04-18 22:31:21 -0400

    contact link fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
             <div>
                 <footer id="footer" uk-scrollspy="target: > div; cls:uk-animation-fade; delay: 500" className="footer uk-section uk-background-dark uk-light uk-padding-remove-bottom">
                     <div className="uk-container">
-                        <h1 className="uk-heading-line uk-text-center"><span>Contact Me</span></h1>
+                        <h1 id="contact" className="uk-heading-line uk-text-center"><span>Contact Me</span></h1>
                         <div className="container uk-padding uk-padding-remove-bottom">
                             <ContactForm />
                         </div>
